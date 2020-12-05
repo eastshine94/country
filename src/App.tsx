d14ec8e1204d464bf75fbbef4387c7dd3403a68a
KimHeeDong <eastshine94@naver.com>
2020-12-05 12:57:10 +0900

    router를 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import {PAGE_PATHS} from '~/constants';
+import { Home } from '~/pages';
 
 class App extends Component {
     render(){
         return(
-            <div>test</div>
+            <Router>
+                <Switch>
+                    <Route path={PAGE_PATHS.HOME} component={Home}/>
+                </Switch>
+            </Router>
         )
     }
 }
